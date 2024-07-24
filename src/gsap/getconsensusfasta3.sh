5022c5f4151e74753813de0722050b0e32f672cd
Sunny Park <sp2307@columbia.edu>
2024-07-24 19:56:37 +0000

    refactored the tool chain for generating consensus sequences as per https://samtools.github.io/bcftools/howtos/consensus-sequence.html

--- src/gsap/getconsensusfasta3.sh
+++ src/gsap/getconsensusfasta3.sh
@@ -2,8 +2,9 @@
 # this conversion only takes care of potential snp's (but not indels) reflected in the resulting fasta file.
 
 refgenomefile=$1
-inputbamfile=$2
+inputvcffile=$2
 resultfilepath=$3
 base_dir=$4
 
-samtools mpileup -uf $refgenomefile $inputbamfile | bcftools call -c | bcftools filter -s LowQual -e '%QUAL<20' - | $4/toolset/bcftools/misc/vcfutils.pl vcf2fq | seqtk seq -A -l 70 - | sed -r "s/(>)(.*)/\1$bam.consensus/g" > $resultfilepath
+#bcftools mpileup -f $refgenomefile $inputbamfile | bcftools call -c | bcftools filter -s LowQual -e '%QUAL<20' - | vcfutils.pl vcf2fq | seqtk seq -A -l 70 - | sed -r "s/(>)(.*)/\1$bam.consensus/g" > $resultfilepath
+cat $refgenomefile | bcftools consensus $inputvcffile > $resultfilepath
